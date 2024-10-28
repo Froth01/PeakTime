c6ee077af1eb4c13922372fb8c598440e96b2e5a
hyun-young <jhy7547@naver.com>
2024-10-28 22:55:34 +0900

    [FEAT][BE][현영] 프리셋 생성 컨트롤러 작성

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/controller/PresetController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/controller/PresetController.java
@@ -1,5 +1,8 @@
 package com.dinnertime.peaktime.domain.preset.controller;
 
+import com.dinnertime.peaktime.domain.preset.entity.Preset;
+import com.dinnertime.peaktime.domain.preset.service.PresetService;
+import com.dinnertime.peaktime.domain.preset.service.dto.request.SavePresetRequestDto;
 import com.dinnertime.peaktime.global.util.CommonSwaggerResponse;
 import com.dinnertime.peaktime.global.util.ResultDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,9 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.attribute.UserPrincipal;
 
@@ -23,6 +24,34 @@ import java.nio.file.attribute.UserPrincipal;
 @RequestMapping("/presets")
 @RequiredArgsConstructor
 public class PresetController {
+
+    private final PresetService presetService;
+
+    // 프리셋 생성
+    @Operation(summary = "프리셋 생성", description = "추가 버튼으로 프리셋 생성하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "프리셋 생성에 성공했습니다.",
+                    content = @Content(schema= @Schema(implementation = ResultDto.class))
+            ),
+            @ApiResponse(responseCode = "500", description = "프리셋 생성에 실패했습니다.",
+                    content= @Content(schema= @Schema(implementation = ResultDto.class))
+            )
+
+    })
+    @CommonSwaggerResponse.CommonResponses
+    @PostMapping
+    public ResponseEntity<?> createPreset(
+            @AuthenticationPrincipal UserPrincipal userPrincipal,
+            @RequestBody SavePresetRequestDto requestDto) {
+        log.info("createPreset 메서드가 호출되었습니다.");
+        log.info("프리셋 생성 : " + requestDto.toString());
+
+        presetService.createPreset(userPrincipal, requestDto);
+        // 임시 세팅
+        return ResponseEntity.status(HttpStatus.OK).body(ResultDto.res(HttpStatus.OK.value(),"프리셋 생성에 성공했습니다."));
+    }
+
+
 
     // 프리셋 조회
     @Operation(summary = "프리셋 전체 조회", description = "프리셋 전체 조회하기")
