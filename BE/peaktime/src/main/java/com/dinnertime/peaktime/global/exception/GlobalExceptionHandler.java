08cc7fe062812bead34a8e101d96d19a6fdbf2cf
hyun-young <jhy7547@naver.com>
2024-10-28 15:52:01 +0900

    [FEAT][BE][현영] error code 형식 통일화(header) 및 security filterchain

---
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,69 @@
+package com.dinnertime.peaktime.global.exception;
+
+import com.dinnertime.peaktime.global.util.ResultDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> handleCustomException(final CustomException ex) {
+        ResultDto<Object> response = ResultDto.res(
+                ex.getErrorCode().getHttpStatus().value(),
+                ex.getMessage()
+        );
+
+        return new ResponseEntity<>(response, ex.getErrorCode().getHttpStatus());
+    }
+
+    // 회원 정보 불일치
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentialsException(final BadCredentialsException ex) {
+        ResultDto<Object> response = ResultDto.res(
+                HttpStatus.UNAUTHORIZED.value(),
+                "회원정보가 일치하지 않습니다."
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    // 400 bad request
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        ResultDto<Object> response = ResultDto.res(
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getBindingResult().getAllErrors().get(0).getDefaultMessage()
+        );
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(final RuntimeException ex) {
+        ResultDto<Object> response = ResultDto.res(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "서버 내부 오류가 발생했습니다. 잠시 후 다시 시도해 주세요."
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(final Exception ex) {
+        log.info(ex.getMessage());
+        ResultDto<Object> response = ResultDto.res(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "서버 내부 오류가 발생했습니다. 잠시 후 다시 시도해 주세요."
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
