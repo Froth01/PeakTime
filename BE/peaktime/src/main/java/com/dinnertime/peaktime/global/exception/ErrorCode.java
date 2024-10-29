f7e16a8f693d4f7c720d23d914eb4983bb8ad226
hyun-young <jhy7547@naver.com>
2024-10-29 22:03:02 +0900

    [FEAT][BE][현영] preset 검증 에러 코드 추가

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/exception/ErrorCode.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/exception/ErrorCode.java
@@ -8,6 +8,7 @@ public enum ErrorCode {
 
     INVALID_PRESET_TITLE_LENGTH("프리셋 타이틀이 6자를 초과하거나 2자 미만일 수 없습니다.",HttpStatus.BAD_REQUEST),
     USER_NOT_FOUND("존재하지 않은 유저입니다.", HttpStatus.NOT_FOUND),
+    PRESET_NOT_FOUND("존재하지 않는 프리셋에 대한 작업입니다.", HttpStatus.NOT_FOUND),
     ;
 
     private final String message;
