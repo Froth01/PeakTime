96e3afa7fe856a72cdc91b4834ee304897d2e08c
hyun-young <jhy7547@naver.com>
2024-10-31 15:12:10 +0900

    [FEAT][BE][현영] memo Entity 및 domain 기본 설정 작성

---
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/memo/controller/MemoController.java
@@ -0,0 +1,21 @@
+package com.dinnertime.peaktime.domain.memo.controller;
+
+import com.dinnertime.peaktime.domain.memo.service.MemoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/memos")
+@RequiredArgsConstructor
+public class MemoController {
+
+    //private final MemoService memoService;
+
+    // 메모 조회
+
+    // 메모 삭제
+
+}
---
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/memo/entity/Memo.java
@@ -0,0 +1,47 @@
+package com.dinnertime.peaktime.domain.memo.entity;
+
+import com.dinnertime.peaktime.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name="memos")
+@ToString
+public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="memo_id")
+    private Long memoId;
+
+    @Column(name="title", nullable = false)
+    private String title;
+
+    @Column(name="create_at", nullable = false)
+    private Date createAt;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    // null 존재 x, true/false primitive 값만 필요해서 boolean으로 설정
+    @Column(name = "is_summaried", nullable = false)
+    private boolean isSummaried;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @Builder
+    private Memo(String title, Date createAt, String content, boolean isSummaried, User user) {
+        this.title = title;
+        this.createAt = createAt;
+        this.content = content;
+        this.isSummaried = isSummaried;
+        this.user = user;
+    }
+
+}
