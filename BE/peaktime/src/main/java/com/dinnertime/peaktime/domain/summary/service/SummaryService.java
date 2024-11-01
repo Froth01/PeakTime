3edc4e9e006c82c3fb695578b0a6e87f72a8ca60
hyun-young <jhy7547@naver.com>
2024-11-01 17:32:41 +0900

    [FEAT][BE][현영] 요약 api 일부 구현

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/summary/service/SummaryService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/summary/service/SummaryService.java
@@ -1,16 +1,37 @@
 package com.dinnertime.peaktime.domain.summary.service;
 
+import com.dinnertime.peaktime.domain.summary.entity.Summary;
 import com.dinnertime.peaktime.domain.summary.repository.SummaryRepository;
+import com.dinnertime.peaktime.domain.user.entity.User;
+import com.dinnertime.peaktime.domain.user.repository.UserRepository;
+import com.dinnertime.peaktime.global.exception.CustomException;
+import com.dinnertime.peaktime.global.exception.ErrorCode;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipal;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class SummaryService {
 
-    // 요약 조회, 저장, 삭제 구현
-//    private final SummaryRepository summaryRepository;
+    // 요약 저장, 삭제 구현
+    private final SummaryRepository summaryRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void deleteSummary(UserPrincipal userPrincipal, Long summaryId) {
+
+        User user = userRepository.findByUserId(1).
+                orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        Summary summary = summaryRepository.findBySummaryId(summaryId)
+                .orElseThrow(() -> new CustomException(ErrorCode.SUMMARY_NOT_FOUND));
+
+        summaryRepository.delete(summary);
+    }
 
 }
