9f43c3667aab24a690cf19e64b44b456c16c013a
1139091SSAFY <iiky2124@gmail.com>
2024-11-01 09:34:25 +0900

    [FIX][BE][동찬] PersistenceContext 생략

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/user/repository/UserRepositoryCustomImpl.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/user/repository/UserRepositoryCustomImpl.java
@@ -1,26 +1,17 @@
 package com.dinnertime.peaktime.domain.user.repository;
 
-import com.dinnertime.peaktime.domain.group.entity.QGroup;
 import com.dinnertime.peaktime.domain.user.entity.QUser;
-import com.dinnertime.peaktime.domain.user.entity.User;
 import com.dinnertime.peaktime.domain.usergroup.entity.QUserGroup;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class UserRepositoryCustomImpl implements UserRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
-
-    @PersistenceContext
-    private EntityManager entityManager;
 
     @Override
     public Long updateIsDeleteByGroupId(Long groupId, Boolean isDelete) {
