419580d39bfe5f6400aee9fba0f9548f2794ac6f
이석범 <dltjrqja01@naver.com>
2024-11-03 22:03:04 +0900

    [FEAT][BE][석범] 하이킹 캘린더 전체 날짜 조회로 수정

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/repository/HikingRepositoryImpl.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/repository/HikingRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.dinnertime.peaktime.domain.hiking.repository;
 
+import com.dinnertime.peaktime.domain.hiking.entity.QCalendar;
 import com.dinnertime.peaktime.domain.hiking.entity.QHiking;
 import com.dinnertime.peaktime.domain.hiking.service.dto.query.HikingCalendarDetailQueryDto;
 import com.dinnertime.peaktime.domain.hiking.service.dto.query.HikingCalendarQueryDto;
@@ -7,6 +8,9 @@ import com.dinnertime.peaktime.domain.user.entity.User;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.DateTemplate;
 import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -20,28 +24,31 @@ import java.util.List;
 public class HikingRepositoryImpl implements HikingRepositoryCustom {
 
     private final QHiking hiking = QHiking.hiking;
+    private final QCalendar calendar = QCalendar.calendar;
     private final JPAQueryFactory queryFactory;
 
 // 날짜만 가져오기
     @Override
     public List<HikingCalendarQueryDto> getCalendar(User user) {
-        // 날짜만 가져오기
-        DateTemplate<Date> startDate = Expressions.dateTemplate(Date.class, "DATE({0})", hiking.startTime);
-
-        //postgresql의 경우 date 타입만 존재하여 date로 반환
         //날짜별로 하이킹 횟수 조회
         return queryFactory.select(Projections.fields(
-                        HikingCalendarQueryDto.class,
-                        startDate.as("date"),
-                        hiking.count().as("intensity")
+                HikingCalendarQueryDto.class,
+                calendar.date.as("date"),
+                        //없는 경우 0을 출력
+                        Expressions.numberTemplate(Integer.class,
+                                "COALESCE(FLOOR(SUM((EXTRACT(EPOCH FROM {0}) - EXTRACT(EPOCH FROM {1})) / 60)), 0)",
+                                hiking.realEndTime, hiking.startTime
+                        ).as("totalMinute")
                 ))
-                .from(hiking)
-                .where(
-                        Expressions.numberTemplate(Integer.class, "EXTRACT(YEAR FROM {0})", hiking.startTime).eq(LocalDate.now().getYear()),
-                        Expressions.numberTemplate(Integer.class, "EXTRACT(MONTH FROM {0})", hiking.startTime).eq(LocalDate.now().getMonthValue()),
-                        hiking.user.eq(user)
+                .from(calendar)
+                .leftJoin(hiking)
+                .on(
+                            calendar.date.eq(Expressions.dateTemplate(Date.class, "DATE_TRUNC('day', {0})", hiking.startTime))
+                                .and(hiking.user.eq(user))
+                                .and(hiking.realEndTime.isNotNull())
                 )
-                .groupBy(startDate)
+                .groupBy(calendar.date)
+                .orderBy(calendar.date.asc())
                 .fetch();
     }
 
@@ -58,8 +65,10 @@ public class HikingRepositoryImpl implements HikingRepositoryCustom {
                 .from(hiking)
                 .where(
                         hiking.user.eq(user),
+                        hiking.realEndTime.isNotNull(),
                         Expressions.dateTemplate(LocalDate.class, "DATE({0})", hiking.startTime).eq(date)
                 )
+                .orderBy(hiking.startTime.asc())
                 .fetch();
     }
 }
