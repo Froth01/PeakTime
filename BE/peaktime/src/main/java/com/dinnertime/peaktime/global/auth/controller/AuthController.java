ebf6ad84cf71386ce558ea838957f50382aa2bec
THIP-Y <nine14141732@gmail.com>
2024-11-04 16:59:47 +0900

    [FEAT][BE][승찬] 임시저장

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/auth/controller/AuthController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/auth/controller/AuthController.java
@@ -14,10 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -34,7 +31,7 @@ public class AuthController {
                     content = @Content(schema = @Schema(implementation = ResultDto.class))),
             @ApiResponse(responseCode = "400", description = "잘못된 형식의 요청입니다.",
                     content = @Content(schema = @Schema(implementation = ResultDto.class))),
-            @ApiResponse(responseCode = "409", description = "이미 존재하는 아이디이거나 이미 존재하는 이메일입니다.",
+            @ApiResponse(responseCode = "409", description = "이미 존재하는 아이디입니다.",
                     content = @Content(schema = @Schema(implementation = ResultDto.class))),
             @ApiResponse(responseCode = "500", description = "일시적인 오류로 회원가입을 할 수 없습니다. 잠시 후 다시 이용해 주세요.",
                     content = @Content(schema = @Schema(implementation = ResultDto.class)))
@@ -47,6 +44,12 @@ public class AuthController {
                 .status(HttpStatus.OK)
                 .body(ResultDto.res(HttpStatus.OK.value(),
                         "회원가입에 성공하였습니다."));
+    }
+
+    // 유저 로그인 아이디 중복 조회
+    @GetMapping("/user-login-id")
+    public ResponseEntity<?> isDuplicatedUserLoginId(@RequestParam String userLoginId) {
+        //
     }
 
 }
