7165bb41e47a49b82f0c9fd873a0afa6d51c3a33
이석범 <dltjrqja01@naver.com>
2024-11-09 14:48:17 +0900

    [FEAT][BE][석범] fetch 추가

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/schedule/entity/Schedule.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/schedule/entity/Schedule.java
@@ -31,7 +31,7 @@ public class Schedule {
     @Column(name = "attention_time", nullable = false)
     private int attentionTime;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "timer_id", nullable = false)
     private Timer timer;
 
