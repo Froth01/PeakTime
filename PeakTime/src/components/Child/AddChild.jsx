2d27146445541c392add0a48d61112defae48564
unknown <iiky2124@gmail.com>
2024-11-10 22:40:36 +0900

    [FIX][EL][동찬] AddChild 페이지 수정

--- PeakTime/src/components/Child/AddChild.jsx
+++ PeakTime/src/components/Child/AddChild.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import groupsApi from "../../api/groupsApi";
 import childrenApi from "../../api/childrenApi";
@@ -12,6 +12,8 @@ function AddChild({ onChangeContent, onChangeGroupList, groupList }) {
   const [childConfirmPassword, setChildConfirmPassword] = useState("");
   const [childNickname, setChildNickname] = useState("");
 
+  const [passwordcheck, setPasswordCheck] = useState(true);
+
   const resetAllInputs = () => {
     setGroupId("");
     setChildLoginId("");
@@ -19,6 +21,11 @@ function AddChild({ onChangeContent, onChangeGroupList, groupList }) {
     setChildConfirmPassword("");
     setChildNickname("");
   };
+
+  // 아이디 중복 확인
+  const handleCheckId = () => {
+
+  }
 
   // 생성하기 클릭
   const handleConfirm = () => {
@@ -54,6 +61,10 @@ function AddChild({ onChangeContent, onChangeGroupList, groupList }) {
       });
   };
 
+  useEffect(() => {
+    setPasswordCheck(childPassword === childConfirmPassword);
+  }, [childPassword, childConfirmPassword]);
+
   // 닫기 클릭
   const handleCancel = () => {
     onChangeContent(null);
@@ -63,59 +74,70 @@ function AddChild({ onChangeContent, onChangeGroupList, groupList }) {
     <div className="absolute left-[40vw] w-[60vw] h-[100vh] bg-white">
       <h2>계정 생성</h2>
 
-      <div>
-        <label htmlFor="childNickname">닉네임</label>
-        <input
-          id="childNickname"
-          name="childNickname"
-          value={childNickname}
-          onChange={(e) => setChildNickname(e.target.value)}
-        />
+      <div className="flex">
+        <div className="flex flex-col text-left">
+          <label htmlFor="childNickname">닉네임</label>
+          <input
+            id="childNickname"
+            name="childNickname"
+            placeholder="닉네임"
+            value={childNickname}
+            onChange={(e) => setChildNickname(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col text-left">
+          <label htmlFor="groupId">소속 그룹</label>
+          <select
+            id="groupId"
+            name="groupId"
+            value={groupId}
+            onChange={(e) => setGroupId(e.target.value)}
+          >
+            <option value="">그룹을 선택하세요.</option>
+            {groupList.map((group) => (
+              <option key={group.groupId} value={group.groupId}>
+                {`${group.groupTitle} (${group.childList.length} / 30)`}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-      <div>
-        <label htmlFor="groupId">소속 그룹</label>
-        <select
-          id="groupId"
-          name="groupId"
-          value={groupId}
-          onChange={(e) => setGroupId(e.target.value)}
-        >
-          <option value="">그룹을 선택하세요.</option>
-          {groupList.map((group) => (
-            <option key={group.groupId} value={group.groupId}>
-              {group.groupTitle}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label htmlFor="childLoginId">아이디</label>
-        <input
-          id="childLoginId"
-          name="childLoginId"
-          value={childLoginId}
-          onChange={(e) => setChildLoginId(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="childPassword">비밀번호</label>
-        <input
-          id="childPassword"
-          name="childPassword"
-          type="password"
-          value={childPassword}
-          onChange={(e) => setChildPassword(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="childConfirmPassword">비밀번호 확인</label>
-        <input
-          id="childConfirmPassword"
-          name="childConfirmPassword"
-          type="password"
-          value={childConfirmPassword}
-          onChange={(e) => setChildConfirmPassword(e.target.value)}
-        />
+
+      <div className="flex">
+        <div className="flex flex-col text-left">
+          <label htmlFor="childLoginId">아이디</label>
+          <input
+            id="childLoginId"
+            name="childLoginId"
+            placeholder="로그인 아이디"
+            value={childLoginId}
+            onChange={(e) => setChildLoginId(e.target.value)}
+          />
+          {/* 이미 존재하는 아이디입니다 추가 예정 */}
+          <button onClick={handleCheckId}>아이디 중복 확인</button>
+        </div>
+        <div className="flex flex-col text-left">
+          <div className="flex flex-col">
+            <label htmlFor="childPassword">비밀번호</label>
+            <input
+              id="childPassword"
+              name="childPassword"
+              placeholder="비밀번호"
+              type="password"
+              value={childPassword}
+              onChange={(e) => setChildPassword(e.target.value)}
+            />
+            <input
+              id="childConfirmPassword"
+              name="childConfirmPassword"
+              placeholder="비밀번호 확인"
+              type="password"
+              value={childConfirmPassword}
+              onChange={(e) => setChildConfirmPassword(e.target.value)}
+            />
+            {!passwordcheck && <div style={{ color: "#F40000" }}>입력한 비밀번호가 일치하지 않습니다.</div>}
+          </div>
+        </div>
       </div>
 
       <button onClick={handleConfirm}>생성하기</button>
