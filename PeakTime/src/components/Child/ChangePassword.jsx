33a6ce9dc3fa01a19fb6780bd7518c40a37cf091
정영일 <bubbleutopia@naver.com>
2024-11-12 13:57:26 +0900

    [DESIGN][EL][영일] 서브계정 수정 css 적용

--- PeakTime/src/components/Child/ChangePassword.jsx
+++ PeakTime/src/components/Child/ChangePassword.jsx
@@ -41,30 +41,48 @@ function ChangePassword({ childId, setPasswordChange }) {
   }, [childPassword, childConfirmPassword]);
 
   return (
-    <div className="flex flex-col">
-      <input
-        type="password"
-        id="childPassword"
-        name="childPassword"
-        value={childPassword}
-        placeholder="새로운 비밀번호"
-        onChange={(e) => handleOnchange("childPassword", e.target.value)}
-      />
-      <input
-        type="password"
-        id="childConfirmPassword"
-        name="childConfirmPassword"
-        value={childConfirmPassword}
-        placeholder="비밀번호 확인"
-        onChange={(e) => handleOnchange("childConfirmPassword", e.target.value)}
-      />
-      {!passwordCheck && (
-        <div style={{ color: "#F40000" }}>
-          입력한 비밀번호가 일치하지 않습니다.
-        </div>
-      )}
-      <button onClick={handleChangePassword}>비밀번호 수정</button>
-      <button onClick={() => setPasswordChange(false)}>닫기</button>
+    <div className="flex justify-evenly absolute left-[25%]">
+      <div className="flex flex-col gap-1">
+        <input
+          type="password"
+          id="childPassword"
+          name="childPassword"
+          value={childPassword}
+          placeholder="새로운 비밀번호"
+          onChange={(e) => handleOnchange("childPassword", e.target.value)}
+          className="h-[60%] w-[13vw] rounded-lg focus:ring-4 focus:ring-[#66aadf] focus:outline-none ps-3"
+        />
+        <input
+          type="password"
+          id="childConfirmPassword"
+          name="childConfirmPassword"
+          value={childConfirmPassword}
+          placeholder="비밀번호 확인"
+          onChange={(e) =>
+            handleOnchange("childConfirmPassword", e.target.value)
+          }
+          className="h-[60%] w-[13vw] rounded-lg focus:ring-4 focus:ring-[#66aadf] focus:outline-none ps-3"
+        />
+        {!passwordCheck && (
+          <div className="text-[#f40000] absolute top-[110%]">
+            입력한 비밀번호가 일치하지 않습니다.
+          </div>
+        )}
+      </div>
+      <div className="flex gap-3 ms-5 h-[50%] self-end">
+        <button
+          onClick={handleChangePassword}
+          className="bg-[#03c777] rounded-xl px-5 py-2 hover:bg-[#02a566] focus:ring-4 focus:ring-[#03c777] text-white font-bold"
+        >
+          비밀번호 수정
+        </button>
+        <button
+          onClick={() => setPasswordChange(false)}
+          className="bg-[#7c7c7c] rounded-xl px-5 py-2 hover:bg-[#5c5c5c] focus:ring-4 focus:ring-[#c5c5c5] text-white font-bold"
+        >
+          닫기
+        </button>
+      </div>
     </div>
   );
 }
--- PeakTime/src/components/Child/UpdateChild.jsx
+++ PeakTime/src/components/Child/UpdateChild.jsx
@@ -6,6 +6,7 @@ import groupsApi from "../../api/groupsApi";
 import childrenApi from "../../api/childrenApi";
 import { updateChildAlertMessage } from "../../utils/Child/UpdateChildAlertMessage";
 import { deleteChildAlertMessage } from "../../utils/Child/DeleteChildAlertMessage";
+import { IoIosArrowDown } from "react-icons/io";
 
 function UpdateChild({
   childId,
@@ -26,6 +27,10 @@ function UpdateChild({
   const [child, setChild] = useState(null);
   const [groupId, setGroupId] = useState(null);
   const [childNickname, setChildNickname] = useState(null);
+
+  // 드롭다운 관련
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
 
   // 서브유저 닉네임 및 그룹 조회
   useEffect(() => {
@@ -110,60 +115,121 @@ function UpdateChild({
   };
 
   return (
-    <div className="absolute left-[40vw] w-[60vw] h-[100vh] bg-white">
-      <div className="flex justify-around">
-        <h2>{childNickname}</h2>
-        <button onClick={() => openDeleteModal()}>계정삭제</button>
-      </div>
+    <div className="absolute left-[43vw] w-[54vw] h-[84vh] my-[3vh] bg-[#333333] bg-opacity-70 rounded-lg p-5 flex flex-col items-center justify-between">
+      <h2 className="text-white text-[30px] font-bold">{childNickname}</h2>
 
-      <div className="flex justify-around">
-        <div className="flex flex-col text-left">
-          <label htmlFor="">닉네임</label>
+      <div className="flex flex-col gap-3 justify-between w-[70%]">
+        <div className="flex flex-col gap-3 text-start w-[40%]">
+          <label htmlFor="" className="text-white font-bold">
+            아이디
+          </label>
           <input
-            id="childNickname"
-            name="childNickname"
-            value={child?.nickname || ""}
-            onChange={(e) => handleChange("childNickname", e.target.value)}
+            id="userLoginId"
+            name="userLoginId"
+            value={child?.userLoginId || ""}
+            disabled
+            className="px-3 py-3 h-[60%] rounded-lg ps-3 bg-opacity-90 text-white font-bold"
           />
         </div>
-        <div className="flex flex-col text-left">
-          <label htmlFor="">소속 그룹</label>
-          <select
-            id="groupId"
-            name="groupId"
-            value={groupId || ""}
-            onChange={(e) => handleChange("groupId", e.target.value)}
-          >
-            {groupList?.map((group) => (
-              <option
-                key={group.groupId}
-                value={group.groupId}
-                disabled={group.childList.length >= 30 ? true : false}
+      </div>
+      <div className="flex flex-col gap-3 justify-between w-[70%]">
+        <div className="flex justify-between w-full">
+          <div className="flex flex-col gap-3 text-start w-[40%]">
+            <label htmlFor="" className="text-white font-bold">
+              닉네임
+            </label>
+            <input
+              id="childNickname"
+              name="childNickname"
+              value={child?.nickname || ""}
+              onChange={(e) => handleChange("childNickname", e.target.value)}
+              className="h-[60%] rounded-lg focus:ring-4 focus:ring-[#66aadf] focus:outline-none ps-3"
+            />
+          </div>
+          <div className="flex flex-col gap-3 text-start w-[40%]">
+            <label htmlFor="groupId" className="text-white font-bold">
+              소속 그룹
+            </label>
+            <div
+              tabIndex={0}
+              onChange={(e) => setGroupId(e.target.value)}
+              className={`relative h-[60%] rounded-lg bg-white border border-gray-300 px-3 py-2 cursor-pointer ${
+                isOpen ? "focus:ring-4 focus:ring-[#66aadf]" : ""
+              }`}
+            >
+              <div
+                onClick={() => setIsOpen(!isOpen)}
+                className="flex justify-between items-center"
               >
-                {group.groupTitle}
-              </option>
-            ))}
-          </select>
+                <p>
+                  {selectedOption
+                    ? selectedOption.groupTitle
+                    : "그룹을 선택해주세요"}
+                </p>
+                <IoIosArrowDown />
+              </div>
+              {isOpen && (
+                <ul
+                  className="absolute left-0 right-0 mt-3 bg-white border
+                border-gray-300 rounded-lg shadow-lg"
+                >
+                  {groupList.map((group, index) => (
+                    <div key={group.groupId}>
+                      <li
+                        onClick={() => {
+                          setGroupId(group.groupId);
+                          setSelectedOption(group);
+                          setIsOpen(false);
+                        }}
+                        className="px-3 py-2 hover:bg-[#66aadf] cursor-pointer rounded-lg hover:font-bold"
+                      >
+                        {`${group.groupTitle} (${group.childList.length} / 30)`}
+                      </li>
+                      {index < groupList.length - 1 && (
+                        <hr className="border-gray-200" />
+                      )}
+                    </div>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex justify-around text-left">
-        <div className="flex flex-col">
-          <label htmlFor="">아이디</label>
-          <input value={child?.userLoginId || ""} disabled />
-        </div>
-        <div></div>
+      <div className="flex gap-3 justify-between w-[70%] relative">
+        <button
+          onClick={handlePassword}
+          className="bg-[#66aadf] rounded-xl px-5 py-2 hover:bg-[#4d90d8] focus:ring-4 focus:ring-[#66aadf] font-bold"
+        >
+          비밀번호변경
+        </button>
+        {passwordChange && (
+          <ChangePassword
+            childId={childId}
+            setPasswordChange={setPasswordChange}
+          />
+        )}
       </div>
-      <div className="flex justify-around">
-        <button onClick={handlePassword}>비밀번호변경</button>
-        <button onClick={handleUpdateChildInfo}>적용하기</button>
-        <button onClick={handleCancel}>닫기</button>
+      <div className="w-full flex justify-end gap-5">
+        <button
+          onClick={() => openDeleteModal()}
+          className="bg-[#f40000] rounded-xl px-5 py-2 hover:bg-[#d60000] focus:ring-4 focus:ring-[#f40000] text-white font-bold"
+        >
+          계정삭제
+        </button>
+        <button
+          onClick={handleUpdateChildInfo}
+          className="bg-[#03c777] rounded-xl px-5 py-2 hover:bg-[#02a566] focus:ring-4 focus:ring-[#03c777] text-white font-bold"
+        >
+          적용하기
+        </button>
+        <button
+          onClick={handleCancel}
+          className="bg-[#7c7c7c] rounded-xl px-5 py-2 hover:bg-[#5c5c5c] focus:ring-4 focus:ring-[#c5c5c5] text-white font-bold"
+        >
+          닫기
+        </button>
       </div>
-      {passwordChange && (
-        <ChangePassword
-          childId={childId}
-          setPasswordChange={setPasswordChange}
-        />
-      )}
     </div>
   );
 }
