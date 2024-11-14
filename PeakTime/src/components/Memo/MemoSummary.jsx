f44bdb674491d36b50ff20758346d685c58ee547
hyun-young <jhy7547@naver.com>
2024-11-14 09:45:19 +0900

    [FEAT][EL][현영] 요약 md로 변환해서 저장시키기

--- PeakTime/src/components/Memo/MemoSummary.jsx
+++ PeakTime/src/components/Memo/MemoSummary.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import dayjs from "dayjs";
 import Swal from "sweetalert2";
 import summariesApi from "../../api/summariesApi";
+import "../../styles/daily-report-custom-swal.css";
 
 function MemoSummary({ data }) {
   const { summaryId, content, updatedAt } = data;
@@ -9,6 +10,64 @@ function MemoSummary({ data }) {
   // 생성날짜 바로 보이게 처리
   const formatDate = (date) => {
     return dayjs(date).format("YY.MM.DD HH:mm:ss");
+  };
+
+  // md 형식으로 변환시키기
+  const convertToMarkdown = (text) => {
+    // 마침표를 기준으로 텍스트를 분할
+    const sentences = text.split(/(?<=\.)\s+/);
+    let markdown = "";
+
+    // 첫 번째 문장은 제목으로 간주
+    markdown += `# ${sentences[0].trim()}\n\n`;
+
+    // 나머지 문장들은 리스트 항목으로 추가
+    for (let i = 1; i < sentences.length; i++) {
+      const trimmedSentence = sentences[i].trim();
+      if (trimmedSentence) {
+        markdown += `- ${trimmedSentence}\n`;
+      }
+    }
+
+    return markdown;
+  };
+
+  // markdown 미리보기
+  const showMarkdownModal = () => {
+    if (!content) {
+      Swal.fire("요약된 내용이 없습니다.", "", "warning");
+      return;
+    }
+
+    const markdownContent = convertToMarkdown(content);
+
+    // SweetAlert2 모달로 Markdown 결과 보여주기
+    Swal.fire({
+      title: "Markdown 미리보기",
+      html: `<pre style="text-align: left; white-space: pre-wrap;">${markdownContent}</pre>`,
+      width: 600,
+      customClass: {
+        popup: "custom-swal-popup",
+      },
+      showCancelButton: true,
+      confirmButtonText: "다운로드",
+      cancelButtonText: "닫기",
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "red",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        downloadMDFile(markdownContent);
+      }
+    });
+  };
+
+  const downloadMDFile = (content) => {
+    const blob = new Blob([content], { type: "text/markdown;charset=utf-8" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = `summary_${summaryId}.md`;
+    link.click();
+    URL.revokeObjectURL(link.href);
   };
 
   // 요약 삭제
@@ -27,6 +86,9 @@ function MemoSummary({ data }) {
   const openDeleteWarn = (summaryId) => {
     Swal.fire({
       title: `해당 요약을 정말로 삭제하시겠습니까?`,
+      customClass: {
+        popup: "custom-swal-popup",
+      },
       icon: "warning",
       showCancelButton: true,
       confirmButtonText: "삭제",
@@ -49,6 +111,9 @@ function MemoSummary({ data }) {
           <p>업데이트 날짜: {formatDate(updatedAt)}</p>
           <button onClick={() => openDeleteWarn(summaryId)}>
             요약 삭제하기
+          </button>
+          <button onClick={showMarkdownModal} style={{ marginLeft: "10px" }}>
+            Markdown 미리보기 및 다운로드
           </button>
         </div>
       ) : (
