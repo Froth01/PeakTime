7c5f807bd8fab9089a4a67178dd86a9c8adabead
이석범 <dltjrqja01@naver.com>
2024-11-14 14:06:54 +0900

    [FEAT][BE][석범] URL 추가시 응답 메시지 추가

---
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/dto/response/SaveUrlPresetResponseDto.java
@@ -0,0 +1,29 @@
+package com.dinnertime.peaktime.domain.preset.service.dto.response;
+
+import com.dinnertime.peaktime.domain.preset.entity.Preset;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SaveUrlPresetResponseDto {
+    private Long presetId;
+    private List<String> blockWebsiteArray;
+
+    @Builder
+    private SaveUrlPresetResponseDto(Long presetId, List<String> blockWebsiteArray) {
+        this.presetId = presetId;
+        this.blockWebsiteArray = blockWebsiteArray;
+    }
+
+    public static SaveUrlPresetResponseDto createSaveUrlPresetResponseDto(Preset preset) {
+        return SaveUrlPresetResponseDto.builder()
+                .presetId(preset.getPresetId())
+                .blockWebsiteArray(preset.getBlockWebsiteArray())
+                .build();
+    }
+}
