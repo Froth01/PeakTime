bacb95b40265c24ef12ffe765bcebd7c9d54f1e6
unknown <iiky2124@gmail.com>
2024-11-16 14:04:51 +0900

    [FIX][EL][동찬] 그룹 추가/삭제 시 버그 수정

--- PeakTime/src/components/Child/ChildList.jsx
+++ PeakTime/src/components/Child/ChildList.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useGroupStore } from "../../stores/GroupStore";
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 import "../../styles/custom-scrollbar.css";
 
 function ChildList() {
   const { groupList, setContent } = useGroupStore();
-  const [isChildListShow, setIsChildListShow] = useState(Array(groupList.length).fill(false));
+  const [isChildListShow, setIsChildListShow] = useState({});
 
-  const setShow = (idx) => {
-    setIsChildListShow((prev) => {
-      const newArray = [...prev];
-      newArray[idx] = !isChildListShow[idx];
-      return newArray;
-    })
-  }
+  // 그룹 목록 변경 시 상태 동기화
+  useEffect(() => {
+    const initialState = {};
+    groupList.forEach((group) => {
+      initialState[group.groupId] = false; // 초기값은 모두 닫힌 상태
+    });
+    setIsChildListShow(initialState);
+  }, [groupList]);
 
-  console.log(isChildListShow)
+  const toggleShow = (groupId) => {
+    setIsChildListShow((prev) => ({
+      ...prev,
+      [groupId]: !prev[groupId], // 해당 그룹의 상태만 토글
+    }));
+  };
 
   return (
     <div className="absolute bg-[#333333] bg-opacity-70 left-[11vw] w-[29vw] h-[84vh] my-[3vh] rounded-lg flex flex-col justify-start items-center text-start p-5">
@@ -27,22 +33,24 @@ function ChildList() {
       </p>
       <div className="flex flex-col gap-5 bg-white text-[20px] w-[25vw] h-[70vh] overflow-y-auto rounded-lg p-5 custom-scrollbar" style={{ scrollbarGutter: "stable", }}>
         {groupList.map((group, groupIdx) => (
-          <div key={groupIdx} className="flex flex-col gap-3">
+          <div key={group.groupId} className="flex flex-col gap-3">
             <button
               className="flex justify-between mx-2 text-left font-bold" 
               onClick={() => { 
                 setContent("updateGroup", group.groupId);
-                setShow(groupIdx);
+                toggleShow(group.groupId);
               }}
             >
-              <div className="text-left truncate w-full">{group.groupTitle} ({group.childList.length} / 30)</div>
-              <>{isChildListShow[groupIdx] ? <SlArrowUp /> : <SlArrowDown />}</>
+              <div className="text-left truncate w-full">
+                {group.groupTitle} ({group.childList.length} / 30)
+              </div>
+              <>{isChildListShow[group.groupId] ? <SlArrowUp /> : <SlArrowDown />}</>
             </button>
 
-            {isChildListShow[groupIdx] && 
+            {isChildListShow[group.groupId] && 
               group.childList.map((child, childIdx) => (
                 <div
-                  key={childIdx}
+                  key={child.userId}
                   className="flex items-center ml-4 text-[18px] w-full"
                 >
                   <button
